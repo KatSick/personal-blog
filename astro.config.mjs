1a707a8a7073a90088a0dde1f87ffe67013dd8bc
Ostap Chervak <ostap.chervak@grammarly.com>
2023-05-29 14:54:54 +0300

    feat(astro): add few integrations

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,21 @@
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
-
-import sitemap from '@astrojs/sitemap';
+import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
+import robotsTxt from "astro-robots-txt";
+import compress from "astro-compress";
+// TODO: mixed content error with HTTPS with prefetch. Need to debug and raise issue https://github.com/withastro/astro/issues?q=%40astrojs%2Fprefetch+https
+// import prefetch from "@astrojs/prefetch";
+import compressor from "astro-compressor";
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
-	integrations: [mdx(), sitemap()],
+  site: "https://blog.katsick.cloud",
+  integrations: [
+    mdx(),
+    sitemap(),
+    robotsTxt(),
+    compress(),
+    // prefetch(),
+    compressor(),
+  ],
 });
